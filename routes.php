9b0ebac2c5a2cc06234eb5ebbd324cea37c64efc
AlexManottupa <manottupaalex376@gmail.com>
2024-10-31 17:43:57 -0500

    primer commit

---
+++ routes.php
@@ -0,0 +1,13 @@
+<?php
+require_once __DIR__.'/router.php';
+
+
+get('/', 'pages/index.php');
+
+get('/user/$id', 'pages/user');
+
+get('/user/$name/$last_name', 'pages/full_name.php');
+
+get('/product/$type/color/$color', 'product.php');
+
+
