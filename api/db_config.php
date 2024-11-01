3352889e64acaa65dc4f76a0d2ae21db38d3d9cc
nextpassword99 <inderscop@gmail.com>
2024-11-01 16:55:31 -0500

    fix: Crear función para establecer la conexión y pasar las variables dentro.

--- api/db_config.php
+++ api/db_config.php
@@ -1,12 +1,17 @@
 <?php
-$host = '23.108.108.119';
-$db = 'socialmedia';
-$user = 'socialmedia';
-$pass = '23VUOvCCZIjkUOH';
+function getConnection()
+{
+  $host = '23.108.108.219';
+  $port = '5432';
+  $dbname = 'socialmedia';
+  $user = 'socialmedia';
+  $pass = '23VUOvCCZIjkUOH';
 
-try {
-  $pdo = new PDO("mysql:host=$host;dbname=$db", $user, $pass);
-  $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-} catch (PDOException $e) {
-  die("Error de conexión: " . $e->getMessage());
+  try {
+    $conn = new PDO("pgsql:host=$host;port=$port;dbname=$dbname", $user, $pass);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    return $conn;
+  } catch (PDOException $e) {
+    die("Error de conexión: " . $e->getMessage());
+  }
 }
