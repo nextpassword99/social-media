1344e2a52be9eae781f29f651651682bfe524ccc
nextpassword99 <inderscop@gmail.com>
2024-11-01 15:52:54 -0500

    add: Formato de respuesta y sistema de publicación.

--- pages/user.php
+++ pages/user.php
@@ -1,47 +1,105 @@
 <?php
+$response = [
+  'user' => [
+    'id' => 12,
+    'name' => 'Edison Pontecil',
+    'profile_image' => 'https://portal.andina.pe/EDPfotografia3/Thumbnail/2021/08/05/000796433W.jpg',
+    'imagen_portada' => 'https://portal.andina.pe/EDPfotografia3/Thumbnail/2021/08/05/000796433W.jpg',
+    'numero_amigos' => 25,
+    'location' => 'Perú, Cusco',
+    'origin' => 'Cusco'
+  ],
+  'amigos' => [
+    [
+      'image_perfil' => 'https://portal.andina.pe/EDPfotografia3/Thumbnail/2021/08/05/000796433W.jpg',
+      'name' => 'Edison Pontecil',
+      'location' => 'Perú, Cusco'
+    ]
+  ],
+  'posts' => [
+    [
+      'image_perfil' => 'https://portal.andina.pe/EDPfotografia3/Thumbnail/2021/08/05/000796433W.jpg',
+      'author' => 'Edison Pontecil',
+      'date' => '31 de octubre de 2024',
+      'content_text' => 'Hola mundo. Este es mi primera publicación.',
+      'content_images' => [
+        'https://upload.wikimedia.org/wikipedia/commons/thumb/d/df/Family_Portrait.jpg/1200px-Family_Portrait.jpg',
+      ],
+      'comentarios' => [
+        'numero_comentarios' => 8,
+        'content_comentarios' => [
+          [
+            'imagen_perfil' => 'https://img.olympics.com/images/image/private/t_1-1_300/f_auto/v1687307644/primary/cqxzrctscdr8x47rly1g',
+            'autor_comentario' => 'Alex Perez',
+            'fecha_comentario' => '31 de octubre de 2024',
+            'contenido_comentario' => 'Buen día.',
+          ]
+        ]
+      ],
+      'reacciones' => [
+        'numero_reacciones' => 12,
+        'usuarios_reacciones' => [
+          'Edison Pontecil',
+          'Edison Pontecil',
+          'Edison Pontecil',
+          'Edison Pontecil',
+        ]
+      ]
+    ],
+  ]
+];
 
-$user_name = $id = 12 ? 'Edison Pontecil' : 'Alex Perez';
 
 
-$title = "Usuario $user_name";
+$user_name = $response['user']['name'];
+$profile_image = $response['user']['profile_image'];
+$title_page = "$user_name | Social Media";
+
 $create_post = file_get_contents(__DIR__ . '/../components/publication/input-post.html');
+$create_post = str_replace('{{title}}', "Usuario $user_name", $create_post);
+$create_post = str_replace('{{imagen_perfil}}', $profile_image, $create_post);
 
+$content_user = $create_post;
+foreach ($response['posts'] as $post) {
+  $post_publicado = file_get_contents(__DIR__ . '/../components/publication/post.html');
+  $post_publicado = str_replace('{{imagen_perfil}}', $post['image_perfil'], $post_publicado);
+  $post_publicado = str_replace('{{autor_post}}', $post['author'], $post_publicado);
+  $post_publicado = str_replace('{{fecha_post}}', $post['date'], $post_publicado);
+  $post_publicado = str_replace('{{content_text}}', $post['content_text'], $post_publicado);
+  $post_publicado = str_replace('{{numero_reacciones}}', $post['reacciones']['numero_reacciones'], $post_publicado);
+  $post_publicado = str_replace('{{numero_comentarios}}', $post['comentarios']['numero_comentarios'], $post_publicado);
+  $post_publicado = str_replace('{{imagen_autor_comentario}}', $post['comentarios']['content_comentarios'][0]['imagen_perfil'], $post_publicado);
+  $post_publicado = str_replace('{{usuario_comentario}}', $post['comentarios']['content_comentarios'][0]['autor_comentario'], $post_publicado);
+  $post_publicado = str_replace('{{contenido_comentario}}', $post['comentarios']['content_comentarios'][0]['contenido_comentario'], $post_publicado);
 
-$create_post = str_replace('{{title}}', $title, $create_post);
-$create_post = str_replace('{{imagen_perfil}}', 'https://storage.googleapis.com/a1aa/image/6SgPkjKVXc6PFNe9bv0qtvqeoM0UeiQoT6NBoqzHzI4ptexOB.jpg', $create_post);
+  $content_visual = '';
+  if (count($post['content_images']) > 0) {
+    foreach ($post['content_images'] as $image_url) {
+      $content_visual .= '<img alt="Post image" src="' . htmlspecialchars($image_url) . '" />';
+    }
+  }
+  $post_publicado = str_replace('{{content_visual}}', $content_visual, $post_publicado);
 
-$post_publicado = file_get_contents(__DIR__ . '/../components/publication/post.html');
-$post_publicado = str_replace('{{imagen_perfil}}', 'https://storage.googleapis.com/a1aa/image/cua7Wu1SxFbgI91fSQUxxY5kpwoU3hOQyf56BdnScRZkmgsTA.jpg', $post_publicado);
-$post_publicado = str_replace('{{autor_post}}', $user_name, $post_publicado);
-$post_publicado = str_replace('{{fecha_post}}', '31 de octubre de 2024', $post_publicado);
-$post_publicado = str_replace('{{content_text}}', 'Hola mundo. Este es mi primera publicación.', $post_publicado);
-$post_publicado = str_replace('{{content_visual}}', 'https://storage.googleapis.com/a1aa/image/cua7Wu1SxFbgI91fSQUxxY5kpwoU3hOQyf56BdnScRZkmgsTA.jpg', $post_publicado);
-$post_publicado = str_replace('{{usuario_actual}}', 'Alex Perez', $post_publicado);
+  $content_user .= $post_publicado;
+}
 
 $content_aside = file_get_contents(__DIR__ . '/../components/aside/details.html');
-$content_aside = str_replace('{{lugar_vive}}', 'Perú, Cusco', $content_aside);
-$content_aside = str_replace('{{lugar_origen}}', 'Cusco', $content_aside);
+$content_aside = str_replace('{{lugar_vive}}', $response['user']['location'], $content_aside);
+$content_aside = str_replace('{{lugar_origen}}', $response['user']['origin'], $content_aside);
 
 $content_header = file_get_contents(__DIR__ . '/../components/user-elements/header.html');
 $content_header = str_replace('{{nombre_usuario}}', $user_name, $content_header);
-
-$content_user = $create_post . $post_publicado;
+$content_header = str_replace('{{imagen_perfil}}', $profile_image, $content_header);
+$content_header = str_replace('{{numero_amigos}}', $response['user']['numero_amigos'], $content_header);
+$content_header = str_replace('{{imagen_fondo}}', $response['user']['imagen_portada'], $content_header);
 
 $template_user = file_get_contents(__DIR__ . '/../components/templates/user.html');
 $template_user = str_replace('{{content}}', $content_user, $template_user);
 $template_user = str_replace('{{aside}}', $content_aside, $template_user);
 $template_user = str_replace('{{perfil_user}}', $content_header, $template_user);
 
-
-$all_content = $template_user;
-
-
 ob_start();
 include __DIR__ . '/../layouts/layout.php';
 $template = ob_get_clean();
-
-
-$template = str_replace('{{content}}', $all_content, $template);
-
-
+$template = str_replace('{{content}}', $template_user, $template);
 echo $template;
