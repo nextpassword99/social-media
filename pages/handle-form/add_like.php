10c5a63cac8d7d7b6446f1f2f5aaf74b8dc1cdec
nextpassword99 <inderscop@gmail.com>
2024-11-02 17:31:46 -0500

    fix: Modificar la forma de obtener los datos en add_like para la petición post

--- pages/handle-form/add_like.php
+++ pages/handle-form/add_like.php
@@ -2,8 +2,10 @@
 require_once __DIR__ . '/../../api/user/like.php';
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-  $user_id = $_POST['user_id'];
-  $post_id = $_POST['post_id'];
+  $data = json_decode(file_get_contents('php://input'), true);
+
+  $user_id = $data['user_id'] ?? null;
+  $post_id = $data['post_id'] ?? null;
 
   if (!$user_id || !$post_id) {
     http_response_code(400);
