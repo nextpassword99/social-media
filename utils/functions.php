f3ee773b0ab74d25a4fb20c3022e42c2587289c3
nextpassword99 <inderscop@gmail.com>
2024-11-02 17:41:25 -0500

    feat: Funciones para extraer y eliminar las etiquetas style

---
+++ utils/functions.php
@@ -0,0 +1,31 @@
+<?php
+/**
+* Extrae todas las etiquetas <style> de un contenido HTML y devuelve
+* en una sola cadena.
+ *
+ * @param string $htmlContent El contenido de HTML para extraer estilos.
+ *
+ * @return string Las etiqueta <style> en una sola cadena.
+ */
+function extractStyles($htmlContent)
+{
+  $pattern = '/<style>(.*?)<\/style>/si';
+  preg_match_all($pattern, $htmlContent, $matches);
+  $consolidatedStyles = isset($matches[0]) ? implode("\n", $matches[0]) : '';
+  return $consolidatedStyles;
+}
+
+
+/**
+ * Elimina todas las etiquetas <style> de un contenido HTML y devuelve
+ * el contenido sin estilos.
+ *
+ * @param string $htmlContent El contenido de HTML para eliminar estilos.
+ *
+ * @return string El contenido de HTML sin estilos.
+ */
+function removeStyles($htmlContent)
+{
+  $pattern = '/<style>(.*?)<\/style>/si';
+  return preg_replace($pattern, '', $htmlContent);
+}
