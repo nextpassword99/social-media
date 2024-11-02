497f7b15d3f46164b85c371dded22a45b9f5277f
nextpassword99 <inderscop@gmail.com>
2024-11-01 22:43:16 -0500

    feat: Ruta principal para mostrar el contenido.

---
+++ pages/index.php
@@ -0,0 +1,51 @@
+<?php
+include_once __DIR__ . '/../api/main/main.php';
+$id = 1;
+$response = buildResponseMain($id);
+
+$user_name = $response['user']['name'];
+$profile_image = $response['user']['profile_image'];
+$title_page = "Social Media";
+
+$create_post = file_get_contents(__DIR__ . '/../components/publication/input-post.html');
+$create_post = str_replace('{{title}}', "Usuario $user_name", $create_post);
+$create_post = str_replace('{{imagen_perfil}}', $profile_image, $create_post);
+
+$content_user = $create_post;
+
+foreach ($response['posts'] as $post) {
+  $post_publicado = file_get_contents(__DIR__ . '/../components/publication/post.html');
+  $post_publicado = str_replace('{{imagen_perfil}}', $post['image_perfil'], $post_publicado);
+  $post_publicado = str_replace('{{autor_post}}', $post['author'], $post_publicado);
+  $post_publicado = str_replace('{{fecha_post}}', $post['date'], $post_publicado);
+  $post_publicado = str_replace('{{content_text}}', $post['content_text'], $post_publicado);
+  $post_publicado = str_replace('{{numero_reacciones}}', $post['reacciones']['numero_reacciones'], $post_publicado);
+  $post_publicado = str_replace('{{numero_comentarios}}', $post['comentarios']['numero_comentarios'], $post_publicado);
+  
+  $comentarios = $post['comentarios']['content_comentarios'] ?? [];
+  $primer_comentario = $comentarios[0] ?? null;
+  
+  $post_publicado = str_replace('{{view_comment}}', $post['comentarios']['numero_comentarios'] > 0 ? 'active' : 'disabled', $post_publicado);
+  $post_publicado = str_replace('{{imagen_autor_comentario}}', $primer_comentario['imagen_perfil'] ?? '', $post_publicado);
+  $post_publicado = str_replace('{{usuario_comentario}}', $primer_comentario['autor_comentario'] ?? '', $post_publicado);
+  $post_publicado = str_replace('{{contenido_comentario}}', $primer_comentario['contenido_comentario'] ?? '', $post_publicado);
+
+  $content_visual = '';
+  if (count($post['content_images']) > 0) {
+    foreach ($post['content_images'] as $image_url) {
+      $content_visual .= '<img alt="Post image" src="' . htmlspecialchars($image_url) . '" />';
+    }
+  }
+  $post_publicado = str_replace('{{content_visual}}', $content_visual, $post_publicado);
+
+  $content_user .= $post_publicado;
+}
+
+$template_user = file_get_contents(__DIR__ . '/../components/templates/index.html');
+$template_user = str_replace('{{content}}', $content_user, $template_user);
+
+ob_start();
+include __DIR__ . '/../layouts/layout.php';
+$template = ob_get_clean();
+$template = str_replace('{{content}}', $template_user, $template);
+echo $template;
