9833e20e1ba158b3ee9705432a991043b83d704f
nextpassword99 <inderscop@gmail.com>
2024-11-06 13:56:13 -0500

    refactor: Eliminar otras rutas y lineas en blanco en routes

--- routes.php
+++ routes.php
@@ -3,11 +3,5 @@ require_once __DIR__.'/router.php';
 
 
 get('/', 'pages/index.php');
-
 get('/user/$id', 'pages/user');
-
-get('/user/$name/$last_name', 'pages/full_name.php');
-
-get('/product/$type/color/$color', 'product.php');
-
 post('/handle-form/like', 'pages/handle-form/add_like.php');
