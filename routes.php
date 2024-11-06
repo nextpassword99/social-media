e0c1e9b18b8c625e5163bc3cf2898cbd09f1ac49
nextpassword99 <inderscop@gmail.com>
2024-11-06 16:38:02 -0500

    feat: Agregar ruta GET para la página de amigos

--- routes.php
+++ routes.php
@@ -4,4 +4,5 @@ require_once __DIR__.'/router.php';
 
 get('/', 'pages/index.php');
 get('/user/$id', 'pages/user');
+get('/user/$id/amigos', 'pages/amigos.php');
 post('/handle-form/like', 'pages/handle-form/add_like.php');
