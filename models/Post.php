d647b26aa3a679474034a1e4d0c5a9a44510867d
nextpassword99 <inderscop@gmail.com>
2024-11-12 07:14:50 -0500

    feat: Agregar método para obtener la cantidad de likes por post id

--- models/Post.php
+++ models/Post.php
@@ -105,6 +105,24 @@ class Post
   }
 
   /**
+   * Devuelve el número de likes de una publicación específica.
+   *
+   * @param int $post_id El ID de la publicación.
+   *
+   * @return int El número total de likes.
+   */
+  public function getCountLikesPorIdPost($post_id): int
+  {
+    $conn = $this->db->getConnection();
+    $query = "SELECT COUNT(*) as count_likes FROM t_likes WHERE publicacion_id = :post_id";
+    $stmt = $conn->prepare($query);
+    $stmt->bindParam(':post_id', $post_id);
+    $stmt->execute();
+
+    return $stmt->fetch(PDO::FETCH_ASSOC)['count_likes'];
+  }
+
+  /**
    * Agrega un like a una publicación.
    *
    * @param int $post_id El ID de la publicación a la que se le va a agregar like.
