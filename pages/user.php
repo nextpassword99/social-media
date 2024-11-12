6a2ee84b88903043f7bb59fa3c85c27c13d44752
nextpassword99 <inderscop@gmail.com>
2024-11-12 13:33:28 -0500

    feat: Agregar comprobación de sesión en la página user

--- pages/user.php
+++ pages/user.php
@@ -1,4 +1,9 @@
 <?php
+require_once __DIR__ . '/../models/Auth.php';
+if (!Auth::validarSession()) {
+  header('Location: /login');
+}
+
 require_once __DIR__ . '/../controllers/UserController.php';
 require_once __DIR__ . '/../layouts/layout.php';
 
