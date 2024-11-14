702f6826ba56093378d58c16538cf5189ef60b5b
Alex Mayhua Quispe <1555784@senati.pe>
2024-11-14 15:46:15 -0500

    fix: Corregir ruta página 404

--- pages/404.php
+++ pages/404.php
@@ -1,13 +1,13 @@
 <?php
-require_once _DIR_ . '/../models/Auth.php';
+require_once __DIR__ . '/../models/Auth.php';
 
 if (!Auth::validarSession()) {
     header('Location: /login');
     exit;
 }
 
-require_once _DIR_ . '/../layouts/layout.php';
-$hmtl = file_get_contents(_DIR_ . '/../views/components/templates/404.html');
+require_once __DIR__ . '/../layouts/layout.php';
+$hmtl = file_get_contents(__DIR__ . '/../views/components/templates/404.html');
 
 $final = new Layout($hmtl, ['titulo_pagina' => '404']);
 $final->render();
