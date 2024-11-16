53d1def65800baffcca55301da87f6cfaa9cb279
nextpassword99 <inderscop@gmail.com>
2024-11-16 13:55:52 -0500

    feat: Agregar métodos remove y extract de scripts en `HtmlHelper`

--- utils/HtmlHelper.php
+++ utils/HtmlHelper.php
@@ -32,4 +32,33 @@ class HtmlHelper
     $pattern = '/<style>(.*?)<\/style>/si';
     return preg_replace($pattern, '', $htmlContent);
   }
+
+  /**
+   * Elimina todas las etiquetas <script> de un contenido HTML y devuelve
+   * el contenido sin scripts.
+   *
+   * @param string $htmlContent El contenido de HTML para eliminar scripts.
+   *
+   * @return string El contenido de HTML sin scripts.
+   */
+  public static function removeScripts($htmlContent)
+  {
+    $pattern = '/<script>(.*?)<\/script>/si';
+    return preg_replace($pattern, '', $htmlContent);
+  }
+  /**
+   * Extrae todas las etiquetas <script> de un contenido HTML y devuelve
+   * un string con todos los scripts encontrados.
+   *
+   * @param string $htmlContent El contenido de HTML para extraer scripts.
+   *
+   * @return string Todas las etiquetas <script> en una sola cadena.
+   */
+  public static function extractScripts($htmlContent)
+  {
+    $pattern = '/<script>(.*?)<\/script>/si';
+    preg_match_all($pattern, $htmlContent, $matches);
+    $consolidatedScripts = isset($matches[0]) ? implode("\n", $matches[0]) : '';
+    return $consolidatedScripts;
+  }
 }
