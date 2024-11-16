910a0c2da64707d2ee6ff5c0bb4bdd3a62642897
nextpassword99 <inderscop@gmail.com>
2024-11-16 13:55:19 -0500

    refactor: Remplazar valor de variable en página user

--- pages/user.php
+++ pages/user.php
@@ -9,7 +9,7 @@ if (!Auth::validarSession()) {
 require_once __DIR__ . '/../controllers/UserController.php';
 require_once __DIR__ . '/../layouts/layout.php';
 
-$html_user = new UserController($usuario_id);
+$html_user = new UserController($_SESSION['usuario_id'] ,$usuario_id);
 $contenido_html = $html_user->render();
 $contenido_final = new Layout($contenido_html, ['titulo_pagina' => 'Hola mundo']);
 $contenido_final->render();
