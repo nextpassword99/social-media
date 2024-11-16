e9feb963c4a258d4ba566cd535c0c40378e3c164
nextpassword99 <inderscop@gmail.com>
2024-11-16 13:45:04 -0500

    feat: Agregar `VideosController`

---
+++ controllers/VideosController.php
@@ -0,0 +1,74 @@
+<?php
+require_once __DIR__ . "/../models/Usuario.php";
+require_once __DIR__ . "/../models/Post.php";
+require_once __DIR__ . "/../components/PostComponent.php";
+require_once __DIR__ . "/../utils/HtmlHelper.php";
+
+class VideosController
+{
+    private $usuario_id_session;
+    private $datos;
+
+    public function __construct($usuario_id_session)
+    {
+        $this->usuario_id_session = $usuario_id_session;
+        $this->cargar_datos();
+    }
+
+    private function cargar_datos()
+    {
+        $usuario = new Usuario($this->usuario_id_session);
+        $datos = [
+            'nombre_completo' => $usuario->getNombre() . ' ' . $usuario->getApellido(),
+            'foto_perfil' => $usuario->getFotoPerfil(),
+        ];
+        $this->datos = $datos;
+    }
+
+    public function render()
+    {
+        $videos = $this->generarPosts();
+        $recursos = $this->cargarRecursos();
+        return $videos . $recursos;
+    }
+    public function generarPosts()
+    {
+        $postObject = new Post($this->usuario_id_session ?? 1);
+        $videos = $postObject->getVideosAleatorios();
+
+        $file_post = file_get_contents(__DIR__ . '/../views/components/publication/post.html');
+        $file_post_estilos = HtmlHelper::extractStyles($file_post);
+        $file_post_sin_estilos = HtmlHelper::removeStyles($file_post);
+
+        $post_html = '';
+        foreach ($videos as $video) {
+            $post = new Post($video['post_id']);
+            $usuario = new Usuario($post->getUsuarioId() ?? 1);
+            $postComponent = new PostComponent(
+                $this->usuario_id_session ?? 1,
+                $this->datos['foto_perfil'],
+                $post->getUsuarioId() ?? 1,
+                $video['post_id'],
+                $usuario->getFotoPerfil(),
+                $this->datos['nombre_completo'],
+                $usuario->getNombre() . " " . $usuario->getApellido(),
+                $post->getFechaPublicacion(),
+                $post->getDescripcion() ?? "",
+                $file_post_sin_estilos,
+                'video',
+            );
+            $post_html .= $postComponent->render();
+        }
+        $post_html .= $file_post_estilos;
+
+        return $post_html;
+    }
+
+    public function cargarRecursos()
+    {
+        $styles_burbuja = HtmlHelper::extractStyles(file_get_contents(__DIR__ . '/../views/components/publication/burbuja-comentario.html'));
+        $script_comentario = HtmlHelper::extractScripts(file_get_contents(__DIR__ . '/../views/components/publication/contenedor-comentario.html'));
+
+        return $script_comentario . $styles_burbuja;
+    }
+}
