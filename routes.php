bb0f8db1af133178ec62dd18bdd7cf325a77cbdb
nextpassword99 <inderscop@gmail.com>
2024-11-16 13:53:33 -0500

    feat: Agregar nueva ruta POST para crear comentarios

--- routes.php
+++ routes.php
@@ -11,5 +11,6 @@ GET('/login/$error', 'pages/login.php');
 POST('/login-process', 'pages/login.php');
 GET('/videos', 'pages/videos.php');
 GET('/videos/$post_id', 'pages/video_id.php');
+POST('/handle-form/comentario', 'pages/handle-form/comentario.php');
 
 any('/404', 'pages/404.php');
