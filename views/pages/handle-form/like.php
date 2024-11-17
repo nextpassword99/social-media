8067a9b146fae9426b19111dcc6b3c4dea6a8930
nextpassword99 <inderscop@gmail.com>
2024-11-16 21:33:25 -0500

    refactor: Asignar valor a `id_usuario` y renombrar a `usuario_id_session`

--- views/pages/handle-form/like.php
+++ views/pages/handle-form/like.php
@@ -5,20 +5,20 @@ require_once __DIR__ . '/../../../models/Post.php';
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
   $data = json_decode(file_get_contents('php://input'), true);
 
-  $user_id = $data['user_id'] ?? null;
+  $usuario_id_session = $_SESSION['usuario_id'] ?? null;
   $post_id = $data['post_id'] ?? null;
 
-  if (!$user_id || !$post_id) {
+  if (!$usuario_id_session || !$post_id) {
     http_response_code(400);
     die();
   }
 
   $response = false;
   $post = new Post($post_id);
-  if (!$post->checkIfLikeExists($post_id, $user_id)) {
-    $response = $post->addLike($post_id, $user_id);
+  if (!$post->checkIfLikeExists($post_id, $usuario_id_session)) {
+    $response = $post->addLike($post_id, $usuario_id_session);
   } else {
-    $response = !$post->deleteLike($post_id, $user_id);
+    $response = !$post->deleteLike($post_id, $usuario_id_session);
   }
 
   header('Content-Type: application/json');
