ae3241b4c95181ec7822ce7eeb6cde0481347649
nextpassword99 <inderscop@gmail.com>
2024-11-17 12:52:05 -0500

    feat: Agregar página `detalles.php` para `/user/$id/detalles`

---
+++ views/pages/user/detalles.php
@@ -0,0 +1,22 @@
+<?php
+session_start();
+require_once __DIR__ . '/../../../models/Auth.php';
+if (!Auth::validarSession()) {
+  header('Location: /login');
+  exit;
+}
+
+require_once __DIR__ . '/../../../controllers/UserController.php';
+require_once __DIR__ . '/../../layouts/layout.php';
+
+$html_user = new UserController($_SESSION['usuario_id'], $usuario_id);
+
+
+$unido = [
+  $html_user->renderHeader(),
+  $html_user->renderInfoUsuario()
+];
+
+$contenido_html = $html_user->renderCompletePage($unido);
+$contenido_final = new Layout($contenido_html, ['titulo_pagina' => 'Detalles de Usuario']);
+$contenido_final->render();
