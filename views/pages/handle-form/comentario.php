185dbb95c9ecb4d5b1374a6cba336e80936ae100
nextpassword99 <inderscop@gmail.com>
2024-11-16 21:38:16 -0500

    refactor: Asignar valor a `id_usuario` y renombrar a `usuario_id_session` en `comentario.php`

--- views/pages/handle-form/comentario.php
+++ views/pages/handle-form/comentario.php
@@ -1,12 +1,13 @@
 <?php
+session_start();
 
 $data = json_decode(file_get_contents('php://input'), true);
 
 require_once __DIR__ . '/../../../models/Post.php';
 
 $post_id = $data['post_id'] ?? null;
-$usuario_id = $data['usuario_id'] ?? null;
+$usuario_id_session = $_SESSION['usuario_id'] ?? null;
 $comentario = $data['comentario'] ?? null;
 
-$esEnviado = Post::setComentario($post_id, $usuario_id, $comentario);
+$esEnviado = Post::setComentario($post_id, $usuario_id_session, $comentario);
 echo json_encode($esEnviado);
