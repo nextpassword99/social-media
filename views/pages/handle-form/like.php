9260e847450fa630e6fddb9b3271cbe55bc2f404
nextpassword99 <inderscop@gmail.com>
2024-11-21 13:36:02 -0500

    refactor: Eliminar comprobación de método de solicitud POST

--- views/pages/handle-form/like.php
+++ views/pages/handle-form/like.php
@@ -14,24 +14,22 @@ if (!Auth::validarSession()) {
 
 require_once __DIR__ . '/../../../models/Post.php';
 
-if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-  $data = json_decode(file_get_contents('php://input'), true);
+$data = json_decode(file_get_contents('php://input'), true);
 
-  $usuario_id_session = $_SESSION['usuario_id'] ?? null;
-  $post_id = $data['post_id'] ?? null;
+$usuario_id_session = $_SESSION['usuario_id'] ?? null;
+$post_id = $data['post_id'] ?? null;
 
-  if (!$usuario_id_session || !$post_id) {
-    http_response_code(400);
-    die();
-  }
-
-  $response = false;
-  $post = new Post($post_id);
-  if (!$post->checkIfLikeExists($post_id, $usuario_id_session)) {
-    $response = $post->addLike($post_id, $usuario_id_session);
-  } else {
-    $response = !$post->deleteLike($post_id, $usuario_id_session);
-  }
-
-  echo json_encode($response);
+if (!$usuario_id_session || !$post_id) {
+  http_response_code(400);
+  die();
 }
+
+$response = false;
+$post = new Post($post_id);
+if (!$post->checkIfLikeExists($post_id, $usuario_id_session)) {
+  $response = $post->addLike($post_id, $usuario_id_session);
+} else {
+  $response = !$post->deleteLike($post_id, $usuario_id_session);
+}
+
+echo json_encode($response);
