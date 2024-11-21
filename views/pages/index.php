c4201794eb29d943fcec48d6362b8eb80058552c
nextpassword99 <inderscop@gmail.com>
2024-11-21 11:51:36 -0500

    refactor: Instanciar controlador Index con instancias de Usuario y DB

--- views/pages/index.php
+++ views/pages/index.php
@@ -1,7 +1,6 @@
 <?php
 session_start();
 require_once __DIR__ . '/../../models/Auth.php';
-require_once __DIR__ . '/../../controllers/indexController.php';
 
 if (!Auth::validarSession()) {
   header('Location: /login');
@@ -9,8 +8,12 @@ if (!Auth::validarSession()) {
 }
 
 require_once __DIR__ . '/../layouts/layout.php';
+require_once __DIR__ . '/../../controllers/indexController.php';
 
-$index = new IndexController($_SESSION["usuario_id"] ?? 1);
+$DB = new DB();
+$Usuario = new Usuario($_SESSION['usuario_id']);
+
+$index = new IndexController($Usuario, $DB);
 $html = $index->render();
 $final = new Layout($html, ['titulo_pagina' => 'Social Media']);
 $final->render();
