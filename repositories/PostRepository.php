1f438b5d48825fbc6002155b3601007dd31960ca
nextpassword99 <inderscop@gmail.com>
2024-11-20 22:56:25 -0500

    feat: Agregar método para insertar nuevo post en la base de datos

--- repositories/PostRepository.php
+++ repositories/PostRepository.php
@@ -8,4 +8,21 @@ class PostRepository
     $this->db = $db;
   }
 
+  public function crearPost($usuario_id, $post_text)
+  {
+    $conn = $this->db->getConnection();
+    $query = "INSERT INTO t_posts (usuario_id, descripcion) VALUES (:usuario_id, :descripcion)";
+    $stmt = $conn->prepare($query);
+    $stmt->bindParam(':usuario_id', $usuario_id, PDO::PARAM_INT);
+    $stmt->bindParam(':descripcion', $post_text, PDO::PARAM_STR);
+    $stmt->execute();
+
+    if ($stmt->rowCount() > 0) {
+      return $conn->lastInsertId();
+    }
+
+    return false;
+  }
+
+
 }
