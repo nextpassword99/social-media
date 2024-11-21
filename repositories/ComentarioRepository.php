4d6fcd5e2c204c8a70d1400d4cdf095b552293ae
nextpassword99 <inderscop@gmail.com>
2024-11-20 22:41:11 -0500

    feat: Añadir método para obtener los comentarios de un post específico

--- repositories/ComentarioRepository.php
+++ repositories/ComentarioRepository.php
@@ -6,4 +6,24 @@ class ComentarioRepository
   {
     $this->db = $db;
   }
+  public function getComentariosPorPostId($post_id)
+  {
+    $conn = $this->db->getConnection();
+    $query = "SELECT c.usuario_id, c.contenido, c.fecha_comentario, 
+                     u.nombre          AS usuario_nombre, 
+                     u.apellido        AS usuario_apellido, 
+                     u.foto_perfil     AS usuario_foto_perfil
+              FROM t_comentarios c
+              JOIN t_usuarios u ON c.usuario_id = u.usuario_id
+              WHERE c.post_id = :post_id
+              ORDER BY c.fecha_comentario DESC
+    ";
+
+    $stmt = $conn->prepare($query);
+    $stmt->bindParam(':post_id', $post_id, PDO::PARAM_INT);
+    $stmt->bindParam(':limite', $post_id, PDO::PARAM_INT);
+    $stmt->execute();
+
+    return $stmt->fetchAll(PDO::FETCH_ASSOC);
+  }
 }
