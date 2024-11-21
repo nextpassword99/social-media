b7c02cea122e1337d3b9e865fe10b1c4f70c01cb
nextpassword99 <inderscop@gmail.com>
2024-11-20 22:49:25 -0500

    feat: Añadir método para insertar una nueva imagen asociada a un post

--- repositories/ImgRepository.php
+++ repositories/ImgRepository.php
@@ -8,4 +8,16 @@ class ImgRepository
     $this->db = $db;
   }
 
+
+  public function agregarImgPost($post_id, $img_url)
+  {
+    $conn = $this->db->getConnection();
+    $query = "INSERT INTO t_imagenes (post_id, url_imagen) VALUES (:post_id, :url_imagen)";
+    $stmt = $conn->prepare($query);
+    $stmt->bindParam(':post_id', $post_id, PDO::PARAM_INT);
+    $stmt->bindParam(':url_imagen', $img_url, PDO::PARAM_STR);
+    $stmt->execute();
+
+    return $stmt->rowCount() > 0;
+  }
 }
