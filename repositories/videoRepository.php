88a0f4989bfda9f022a89c701f66c0aed72deef3
nextpassword99 <inderscop@gmail.com>
2024-11-20 23:09:14 -0500

    feat: Añadir método para insertar videos asociados a los posts

--- repositories/videoRepository.php
+++ repositories/videoRepository.php
@@ -8,4 +8,17 @@ class VideoRepository
     $this->db = $db;
   }
 
+
+  public function agregarVideoPost($post_id, $video_url)
+  {
+    $conn = $this->db->getConnection();
+    $query = "INSERT INTO t_videos (post_id, url_video) VALUES (:post_id, :url_video)";
+    $stmt = $conn->prepare($query);
+    $stmt->bindParam(':post_id', $post_id, PDO::PARAM_INT);
+    $stmt->bindParam(':url_video', $video_url, PDO::PARAM_STR);
+    $stmt->execute();
+
+    return $stmt->rowCount() > 0;
+  }
+
 }
